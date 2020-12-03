fdc1c1893898eb2d66d0ec9ef56e17763679576f
Krot Eval <38257723+4n70w4@users.noreply.github.com>
2020-12-03 14:29:13 +0300

    first version

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,34 @@
 <template>
-    <panel-item :field="field" />
+    <panel-item :field="field">
+        <template slot="value">
+            <katex-element :expression="field.value" />
+        </template>
+    </panel-item>
 </template>
 
 <script>
+//    import Vue from 'vue';
+//    import VueKatex from 'vue-katex';
+    import KatexElement from './KatexElement'
+
+
+//import { KatexElement } from "vue-katex";
+//    Vue.use(VueKatex);
+/*
+    Nova.booting((Vue, router) => {
+        Vue.use(KatexElement);
+    });
+*/
 export default {
+
+    components: {
+        KatexElement
+    },
+
     props: ['resource', 'resourceName', 'resourceId', 'field'],
 }
 </script>
+
+<style lang="sass">
+    @import "../../../node_modules/katex/dist/katex.min.css"
+</style>
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,30 +1,18 @@
 <template>
     <div>
-    <div v-katex="'\\frac{a_i}{1+x}'"></div>
-    <div v-katex:display="'\\frac{a_i}{1+x}'"></div>
-    <div v-katex="{ expression: '\\frac{a_i}{1+x}', options: { throwOnError: false }}"></div>
-    <div v-katex:auto>
-        \(\frac{a_i}{1+x}\)
-    </div>
-    <div v-katex:auto="{ options }">
-        \(\frac{a_i}{1+x}\)
-    </div>
-    <katex-element expression="'\\frac{a_i}{1+x}'"/>
+        <katex-element :expression="field.value" />
     </div>
 </template>
 
 <script>
-    import Vue from 'vue';
-    import VueKatex from 'vue-katex';
-    import 'katex/dist/katex.min.css';
-
-    Vue.use(VueKatex, {
-        globalOptions: {
-            //... Define globally applied KaTeX options here
-        }
-    });
+import KatexElement from './KatexElement'
 
 export default {
+
+    components: {
+        KatexElement
+    },
+
     props: ['resourceName', 'field'],
 }
 </script>
